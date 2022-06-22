9d87bb340ac73eab5f6d186094af41fc115b9646
Nathan Truyts <78803504+Nathtruyts@users.noreply.github.com>
2022-06-22 08:57:37 -0300

    Update conectarBD.py

--- src/conectarBD.py
+++ src/conectarBD.py
@@ -4,7 +4,7 @@ def conectar():
     myRedis = redis.Redis(
     host='redis-10018.c299.asia-northeast1-1.gce.cloud.redislabs.com',
     port='10018',
-    password='nerve1'
+    password='password'
     )
     return myRedis
 
